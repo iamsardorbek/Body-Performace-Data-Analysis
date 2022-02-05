d1de9f4b55ad5a2eabd18aae3c23300af1139357
Sardorbek Khujaev <58874777+iamsardorbek@users.noreply.github.com>
2022-02-05 17:15:11 +0500

    add dataset to dir, import the libs for EDA

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+# import seaborn as sns
+# import matplotlib as mp
+
+
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data = pd.read_csv("//bodyPerformanceDataset.csv")
+    data.head()
+    # print_hi('PyCharm')
